5a6c43a3d38f14a56b806dc13aa1140636f9f430
gonzzoh <gmeza0118@gmail.com>
2023-12-13 15:47:46 -0500

    Click Functionality Implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,44 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Board from './board'
-import Form from './form'
 function App() {
+  const [name, setName] = useState('');
+
+  const [player1, setPlayer1] = useState(["Player 1", []]);
+  const [player2, setPlayer2] = useState(['Player 2', []]);
+
+  const handleControlledSubmit = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+    const obj = Object.fromEntries(formData);
+    console.log(formData.entries())
+    e.target.reset()
+  };
+  const handleChange = (e) => {
+    const newVal = e.target.value;
+    // if (e.target.id === player1) {
+    //     setPlayer1(e.target.value);
+    // } else if (e.target.id === player2) {
+    //     setPlayer2(e.target.value);
+    // }
+    setName(prev => prev = newVal)
+    console.log(newVal)
+  }
+
+
   return (
-    <main>
-      <Form />
-      <Board />
-    </main>
+    <>
+      <form onSubmit={handleControlledSubmit}>
+        <label htmlFor="name"></label>
+        <select>
+          <option value="player1">{player1}</option>
+          <option value="player2">{player2}</option>
+        </select>
+        <input id="name" value={name} onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
+      <Board {...{ player1, player2 }}/>
+    </>
   )
 }
 export default App
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,24 +1,32 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import "../styles/board.css"
 
-export default function Board() {
+export default function Board(players) {
+    const [curPlayer, setCurPlayer] = useState('X');
+    // console.log(players)
     const handleClick = (e) => {
-        console.log(e.target.id)
+        let tile = e.target
+        tile.textContent = curPlayer
+        curPlayer === "X" ? tile.setAttribute('data-set-player', players.player1[0]) : tile.setAttribute('data-set-player', players.player2[0])
+        setCurPlayer(curPlayer === 'X' ? 'O' : 'X')
+        console.log(curPlayer, tile)
     }
+
     return (
         <>
             <h1>Game Board</h1>
             <section id="gameBoard">
-                <div className="cell" id="1" onClick={handleClick}>1</div>
-                <div className="cell" id="2" onClick={handleClick}>2</div>
-                <div className="cell" id="3" onClick={handleClick}>3</div>
-                <div className="cell" id="4" onClick={handleClick}>4</div>
-                <div className="cell" id="5" onClick={handleClick}>5</div>
-                <div className="cell" id="6" onClick={handleClick}>6</div>
-                <div className="cell" id="7" onClick={handleClick}>7</div>
-                <div className="cell" id="8" onClick={handleClick}>8</div>
-                <div className="cell" id="9" onClick={handleClick}>9</div>
+                <div className="cell" id="1" data-set-player="" onClick={handleClick}></div>
+                <div className="cell" id="2" data-set-player="" onClick={handleClick}></div>
+                <div className="cell" id="3" data-set-player="" onClick={handleClick}></div>
+                <div className="cell" id="4" data-set-player="" onClick={handleClick}></div>
+                <div className="cell" id="5" data-set-player="" onClick={handleClick}></div>
+                <div className="cell" id="6" data-set-player="" onClick={handleClick}></div>
+                <div className="cell" id="7" data-set-player="" onClick={handleClick}></div>
+                <div className="cell" id="8" data-set-player="" onClick={handleClick}></div>
+                <div className="cell" id="9" data-set-player="" onClick={handleClick}></div>
             </section>
         </>
     )
 }
+

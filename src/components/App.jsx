8c6e368c5aa57f036c1ccb87cc22f404dbd5e9c3
gonzzoh <gmeza0118@gmail.com>
2023-12-12 22:15:56 -0500

    Implemented Props, Styled, Created Gameboard

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import Board from './board'
-
+import Form from './form'
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [name, setName] = useState('');
-
-  const [player1, setPlayer1] = useState("Player 1");
-  const [player2, setPlayer2] = useState('Player 2');
-
-  const handleControlledSubmit = (e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target);
-    const obj = Object.fromEntries(formData.entries());
-    console.log("input:", obj)
-    e.target.reset()
-  };
-  const handleChange = (event) => {
-    const newVal = event.target.value;
-    // if (event.target.id === player1) {
-    //     setPlayer1(event.target.value);
-    // } else if (event.target.id === player2) {
-    //     setPlayer2(event.target.value);
-    // }
-    setName(prev => prev = newVal)
-    console.log(event.target.value)
-  }
-
-
   return (
-    <>
-      <form onSubmit={handleControlledSubmit}>
-        <label htmlFor="name"></label>
-        <select>
-          <option value="player1">{player1}</option>
-          <option value="player2">{player2}</option>
-        </select>
-        <input id="name" value={name} onChange={handleChange} />
-        <button>Submit</button>
-      </form>
-    </>
+    <main>
+      <Form />
+      <Board />
+    </main>
   )
 }
-
 export default App
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,9 +1,24 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import "../styles/board.css"
 
 export default function Board() {
-
+    const handleClick = (e) => {
+        console.log(e.target.id)
+    }
     return (
-        <section id="gameBoard">
-        </section>
+        <>
+            <h1>Game Board</h1>
+            <section id="gameBoard">
+                <div className="cell" id="1" onClick={handleClick}>1</div>
+                <div className="cell" id="2" onClick={handleClick}>2</div>
+                <div className="cell" id="3" onClick={handleClick}>3</div>
+                <div className="cell" id="4" onClick={handleClick}>4</div>
+                <div className="cell" id="5" onClick={handleClick}>5</div>
+                <div className="cell" id="6" onClick={handleClick}>6</div>
+                <div className="cell" id="7" onClick={handleClick}>7</div>
+                <div className="cell" id="8" onClick={handleClick}>8</div>
+                <div className="cell" id="9" onClick={handleClick}>9</div>
+            </section>
+        </>
     )
 }
